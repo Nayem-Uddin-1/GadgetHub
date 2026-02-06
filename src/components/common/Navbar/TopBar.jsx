408593a6e50141d8b4dfa9586f183f516a9e34aa
Nayem-Uddin-1 <nayem.origin@gmail.com>
2026-02-06 23:43:58 +0600

    complete TopBar section

--- src/components/common/Navbar/TopBar.jsx
+++ src/components/common/Navbar/TopBar.jsx
@@ -3,34 +3,50 @@ import { Link } from 'react-router-dom'
 import Container from '../Container/Container'
 import SearchBar from './searchbar/SearchBar'
 import { BiDollar } from "react-icons/bi";
+import GroupButton from './buttonGroup/GroupButton';
+import { FaRegUser } from "react-icons/fa";
+import { CgMail } from "react-icons/cg";
+import { RiBloggerLine } from "react-icons/ri";
+
+
+
 
 
 function TopBar() {
   return (
     <section className='bg-[#092A53] text-[36px] '>
       <Container>
-        <div className='flex items-center py-5'>
-          <Link to={"/"} className='logoFont text-white ' >
+        <div className='flex items-center gap-30 py-5'>
+          <Link to={"/"} className='logoFont text-white text-5xl ' >
             Gadget<span className='text-red-500' >.</span>Hub
           </Link>
+          <div className="flex justify-between items-center gap-10 ">
 
-          <SearchBar />
+            <SearchBar />
 
-          {/* country img */}
+            {/* country img */}
+            <div className='flex gap-3'>
+              <button className="flex items-center justify-center w-12 h-12 border-2 border-white rounded-lg bg-transparent hover:bg-white/10 transition-colors">
+                <img
+                  src="https://flagcdn.com/w80/gb.png"
+                  alt="UK Flag"
+                  className="w-7 h-auto"
+                />
+              </button>
 
-          <button className="flex items-center justify-center w-12 h-12 border-2 border-white rounded-lg bg-transparent hover:bg-white/10 transition-colors">
-            <img
-              src="https://flagcdn.com/w80/gb.png"
-              alt="UK Flag"
-              className="w-7 h-auto"
-            />
-          </button>
+              <button className="flex items-center justify-center w-12 h-12 border-2 border-white rounded-lg bg-transparent hover:bg-white/10 transition-colors" >
+                <BiDollar className="text-white text-2xl" />
+              </button>
+            </div>
 
-          <button className="flex items-center justify-center w-12 h-12 border-2 border-white rounded-lg bg-transparent hover:bg-white/10 transition-colors" >
-            <BiDollar className="text-white text-2xl" />
-          </button>
+            {/* buttons */}
+            <div className='flex gap-3'>
+              <GroupButton icon={<FaRegUser />} text="Account" />
+              <GroupButton icon={<CgMail />} text="Contact" />
+              <GroupButton icon={<RiBloggerLine />} text="Blog" />
+            </div>
 
-          
+          </div>
 
 
 
--- src/components/common/Navbar/buttonGroup/GroupButton.jsx
+++ src/components/common/Navbar/buttonGroup/GroupButton.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { FaRegUser } from "react-icons/fa";
 
-function GroupButton() {
-  return (
-    <div>GroupButton</div>
-  )
+
+function GroupButton({icon,text}) {
+    return (
+        <div>
+
+            <button className='text-[20px] text-black p-3 rounded flex items-center gap-2 bg-white '>
+                <span>{icon}</span>
+                 <h3>{text}</h3>
+            </button>
+
+
+        </div>
+    )
 }
 
 export default GroupButton
